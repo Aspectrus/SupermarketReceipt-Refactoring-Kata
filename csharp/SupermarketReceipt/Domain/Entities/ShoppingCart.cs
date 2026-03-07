b6c0d860298898be6dc1a7834b009a637066219a
Aspectrus <Aspectrus>
2026-03-07 13:37:50 +0200

    Remove redundant _productQuantities dictionary and compute product quantities from items on demand

--- csharp/SupermarketReceipt/Domain/Entities/ShoppingCart.cs
+++ csharp/SupermarketReceipt/Domain/Entities/ShoppingCart.cs
@@ -1,26 +1,30 @@
-using SupermarketReceipt.Domain.Entities.Offers;
-using SupermarketReceipt.Domain.Interfaces;
 using SupermarketReceipt.Domain.ValueObjects;
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Collections.ObjectModel;
 
 namespace SupermarketReceipt.Domain.Entities
 {
     public class ShoppingCart
     {
         private readonly List<ProductQuantity> _items = new List<ProductQuantity>();
-        private readonly Dictionary<Product, Quantity> _productQuantities = new Dictionary<Product, Quantity>();
+
+        public IReadOnlyDictionary<Product, Quantity> GetProductQuantities()
+        {
+            var result = new Dictionary<Product, Quantity>();
+            foreach (var item in _items)
+            {
+                if (result.TryGetValue(item.Product, out var existing))
+                    result[item.Product] = existing.Add(item.Quantity);
+                else
+                    result[item.Product] = item.Quantity;
+            }
+            return result;
+        }
 
 
         public List<ProductQuantity> GetItems()
         {
             return new List<ProductQuantity>(_items);
-        }
-        public IReadOnlyDictionary<Product, Quantity> GetProductQuantities()
-        {
-            return _productQuantities.AsReadOnly();
         }
 
         public void AddItem(Product product)
@@ -34,17 +38,9 @@ namespace SupermarketReceipt.Domain.Entities
         {
             if (product.Unit != quantity.Unit)
                 throw new ArgumentException($"Product unit {product.Unit} doesn't match quantity unit {quantity.Unit}");
-            _items.Add(new ProductQuantity(product, quantity));
-            if (_productQuantities.ContainsKey(product))
-            {
-                var newAmount = _productQuantities[product].Add(quantity);
-                _productQuantities[product] = newAmount;
-            }
-            else
-            {
-                _productQuantities.Add(product, quantity);
-            }
 
+            _items.Add(new ProductQuantity(product, quantity));
         }
+
     }
 }
